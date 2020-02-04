d499b95be1d509f4ff79ebff260e205837bf77a3
iamfenton <46457420+iamfenton@users.noreply.github.com>
2020-02-04 22:28:06 +0000

    A program that showcases how variables work in Java.

---
+++ variables.java
@@ -0,0 +1,38 @@
+public class variables {
+  public static void main(String[] args) {
+
+    // creating a string variable called 'name' and assigning the value
+    // 'Fenton' to it.
+
+    String name = "Fenton";
+
+    // creating an integer variable called 'date' and assigning the value
+    // of '1410' to it.
+
+    int date = 1410;
+
+    // creating a character variable called 'grade' and assigning the value
+    // of 'A' to it.
+
+    char grade = 'A';
+
+    // creating a floating point variable called 'price' and assigning the
+    // value of '7.00' to it.
+
+    float price = 7.00f;
+
+    // creating a boolean variable called 'earthIsFlat' and assigning the
+    // value of 'false' to it.
+
+    boolean earthIsFlat = false;
+
+    // printing all of the variables to the terminal.
+
+    System.out.println(name);
+    System.out.println(date);
+    System.out.println(grade);
+    System.out.println(price);
+    System.out.println(earthIsFlat);
+    
+  }
+}
