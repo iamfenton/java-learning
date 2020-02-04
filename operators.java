07a881379ed359861bee21c5258e21c611ea12d8
iamfenton <46457420+iamfenton@users.noreply.github.com>
2020-02-04 22:45:11 +0000

    Program that showcases basic operators in Java.

---
+++ operators.java
@@ -0,0 +1,33 @@
+public class operators {
+	public static void main(String[] args) {
+
+		// creating an integer variable called 'addition'
+		// and assigning the operation of "5 + 5" to it.
+
+		int addition = 5 + 5;
+
+		// creating an integer variable called 'multiplication'
+		// and assigning the opeartion of "2 * 4" to it.
+
+		int multiplication = 2 * 4;
+
+		// creating an integer variable called 'subtraction'
+		// and assigning the operation of "10 - 5" to it.
+
+		int subtraction = 10 - 5; 
+
+		// creating an integer variable called 'division'
+		// and assiging the operation of "100 / 2" to it.
+
+		int division = 100 / 2;
+		
+		// printing the results of operations stored in
+		// the variables to the terminal.
+
+		System.out.println(addition); 
+		System.out.println(multiplication);
+		System.out.println(subtraction);
+		System.out.println(division);
+
+	}
+}
