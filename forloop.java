ffc470a3e95f82af79ce33369c000b0425c9740b
iamfenton <46457420+iamfenton@users.noreply.github.com>
2020-02-05 09:16:02 +0000

    Program that shows how for loops work in Java.

---
+++ forloop.java
@@ -0,0 +1,13 @@
+public class forloop {
+	public static void main(String[] args) {
+
+		// creating an integer variable called number
+		// and giving it the value of '0'. for the number
+		// being less than '6' add '1' to the number variable
+		// each loop and print the variable. 
+
+	for (int number = 0; number < 6; number++) {
+			System.out.println(number);
+		}
+	}
+}
