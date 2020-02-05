80f80329ed943aa2397d6c45cd96df07ab9f067e
iamfenton <46457420+iamfenton@users.noreply.github.com>
2020-02-05 10:16:30 +0000

    Program that showcases how methods work in Java.

---
+++ methods.java
@@ -0,0 +1,20 @@
+public class methods {
+
+	// methods are essentially functions.
+	// methods must be declared in a class.
+	// the method below will print "Hello, world!"
+	// to the terminal once called.
+
+	static void helloMethod() {
+		System.out.println("Hello, world!");
+	}
+
+	public static void main (String[] args) {
+
+		// calling the 'helloMethod()' method.
+		// the code below will write "Hello, world!"
+		// to the terminal.
+
+		helloMethod();
+	}
+}
