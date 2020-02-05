14628df46b055fe46c1286b180d4ec6fce970e2c
iamfenton <46457420+iamfenton@users.noreply.github.com>
2020-02-05 01:31:36 +0000

    A program that shows how 'while' loops work in Java.

---
+++ whileloop.java
@@ -0,0 +1,21 @@
+public class whileloop {
+	public static void main(String[] args) {
+
+		// creating an integer variable called 'num'
+		// and giving it the value of '0'.
+
+		int num = 0;
+
+		// while the 'number' variable is less than '6' then
+		// write the value of the 'number' variable to the screen
+		// and add '1' to the value each time.
+
+		while (num < 6) {
+			System.out.println(num); 
+			
+			// adding '1' to the value of num.
+
+			num++; 
+		}
+	}
+}
