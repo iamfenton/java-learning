9062e7443af0ca1b35bf3fed28e2a3de738c04e6
iamfenton <46457420+iamfenton@users.noreply.github.com>
2020-02-06 01:04:57 +0000

    Updated source file to show various different examples of methods.

--- methods.java
+++ methods.java
@@ -9,6 +9,25 @@ public class methods {
 		System.out.println("Hello, world!");
 	}
 
+	// the method below takes a string variable
+	// called 'name' as the parameter. once the
+	// name is passed into the method it is 
+	// concatenated with a phrase which reads 
+	// "is epic!".
+
+	static void isEpic(String name) {
+		System.out.println(name + " is epic!");
+	}
+
+	// the method below takes an integer variable 
+	// in the parentheses then adds '5' to it.
+	// the method then returns the result and 
+	// prints it to the terminal.
+
+	static int additionCalc(int num) {
+		return 5 + num;
+	}
+
 	public static void main (String[] args) {
 
 		// calling the 'helloMethod()' method.
@@ -16,5 +35,22 @@ public class methods {
 		// to the terminal.
 
 		helloMethod();
+
+		// calling the 'isEpic()' method.
+		// the code below will take the name
+		// in the parentheses and concatenate
+		// "is epic!" at the end. it will output
+		// to the terminal "Fenton is epic!".
+
+		isEpic("Fenton");
+		isEpic("Jamie-lee");
+
+		// calling the 'additionCalc' method.
+		// the code below will take an integer
+		// in the parentheses and add 5 to it.
+		// it will output to the terminal '10'.
+
+		System.out.println(additionCalc(10));
+
 	}
 }
